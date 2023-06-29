2122df789c3a018cda5348ae1d5a05297eefd37c
Hiral Kakrecha <hiral.kakrecha@yudiz.com>
2023-06-29 12:47:41 +0530

    graphQL demo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import DisplayLocations from "./views/Display";
+
+
+function App() {
+  return (
+    <div>
+      <h2>GraohQL Demo</h2>
+      <DisplayLocations />
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/Display/index.js
@@ -0,0 +1,41 @@
+import { useQuery, gql } from "@apollo/client";
+
+
+function DisplayLocations() {
+
+  const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
+  const { loading, error, data} = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+  
+
+  return (
+    <>
+      {
+        data.locations.map(({ id, name, description, photo }) => (
+          <div key={id}>
+            <h3>{name}</h3>
+            <img width="400" height="250" alt="location-reference" src={photo} />
+            <br />
+            <b>About this location:</b>
+            <p>{description}</p>
+            <br />
+          </div>
+        ))
+      }       
+    </>
+  )
+}
+
+export default DisplayLocations;
